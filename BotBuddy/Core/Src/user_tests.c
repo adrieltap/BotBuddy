017b8373d33c45b461e08aa12272874c246e068f
taparra2 <ataparra20@gmail.com>
2024-03-16 00:52:07 -0500

    first test commit of BotBuddy

---
+++ BotBuddy/Core/Src/user_tests.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include "user_tests.h"
+
+/* Function prototype */
+
+void user_program(void) {
+
+	while(1) {
+
+	}
+}
---
+++ BotBuddy/Core/Src/user_tests.h
@@ -0,0 +1,13 @@
+/*
+ *User test header file
+ *
+ *
+ *
+ * */
+
+
+#include "main.h"
+
+
+extern void user_program(void);
+
